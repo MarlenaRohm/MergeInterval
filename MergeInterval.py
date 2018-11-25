ba151529b093fb2200279aa5ca05e8163ea589d6
MarlenaRohm <45336184+MarlenaRohm@users.noreply.github.com>
2018-11-25 18:05:18 +0100

    Add files via upload

--- MergeInterval.py
+++ MergeInterval.py
@@ -1,17 +1,18 @@
 def MERGE(*args):
-    if len(args) < 2:
-        print("Nur möglich mit mindestens zwei Intervallen")
+    if len(args) < 2:     #if not more then 2 intervals are given, the script stops
+        print("Only possible with two intervals as input")
         return
-    args=sorted(args)
-    new=None
-    newlist = []
+    args=sorted(args) #sorts the arguments
+    new=None #temporarily saves a merged interval
+    newlist = [] #this will be the list of merged intervals
     length = True 
-    x=0
-    y=0
+    x=0 
+    y=0 
     while (length):
         for arg in args:
             y=y+1
             x=x+1
+            #last argument (or if merged before, the last merge) is added into the newlist without being compared again and script stops
             if len(args) == y:
                 if new is not None:
                     newlist.append(new)
@@ -20,29 +21,30 @@ def MERGE(*args):
                 length=False
                 return newlist
             else:
-                if new is None:
+                #if nothing is merged yet "new" will be none. The current and next argument will be compared
+                if new is None: 
                     f=arg
-                    s=args[x]
+                    s=args[x] 
                     new=domerge(f,s)
-                    if (new is None) and (f not in newlist):
+                    #if there was nothing to merge (None as output from domerge()) the current argument will be saved in "newlist" without creating duplicates
+                    if (new is None) and (f not in newlist): 
                         newlist.append(f)
                 else:
-                    f=new
+                    #if there was something merged before, it will be used as new current argument and will be compared with next argument
+                    f=new 
                     s=args[x]
                     new=domerge(f,s)
                     if (new is None) and (f not in newlist):
                         newlist.append(f)
                         
 
-
+#actual merge function
 def domerge(f,s):
-    for i in range(f[0],f[-1]+1):
-        for j in range(s[0],s[-1]+1):
+    for i in range(f[0],f[-1]+1): #goes through the interval of current argument/previously merged
+        for j in range(s[0],s[-1]+1): #goes through the interval of the next argument
+            #if intervals overlap or are consecutive, first digit in current argument becomes first number in new merge (it was sorted previously), highest number of arguments will be last number in new merge
             if i == j:
-                if f[0]<=s[0]:
-                    a=f[0]
-                else:
-                    a=s[0]
+                a=f[0] 
                 if f[-1]>=s[-1]:
                     b=f[-1]
                 else:
@@ -50,17 +52,16 @@ def domerge(f,s):
                 new=[a,b]
                 return new
             elif (i+1 == j) or (i-1 == j):
-                if f[0]<=s[0]:
-                    a=f[0]
-                else:
-                    a=s[0]
+                a=f[0]
                 if f[-1]>=s[-1]:
                     b=f[-1]
                 else:
                     b=s[-1]
                 new=[a,b]
                 return new
-            elif i == f[-1]:
+            #if last is reached without overlap/consecutive stop without output
+            elif i == f[-1]: 
                 return None
 
+#example:
 MERGE([25,30],[2,19],[14,23],[4,8]) 
